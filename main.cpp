59f79cb32e9c181135e26b51b26e7b8e1397cfd9
Oleg Mironov <osmironov@gmail.com>
2018-06-23 18:36:56 +0300

    const version

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
+#include <cstring>
 #include <iostream>
 #include "hello.h"
+#include "version.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << say_hello() << std::endl;
+    if (argc == 1)
+    {
+        std::cout << say_hello() << std::endl;
 
-    return 0;
+        return 0;
+    }
+
+    if (argc == 2 && std::strcmp(argv[1], "--version") == 0)
+    {
+        std::cout << "A Hello World project. Version: " << PROJECT_VERSION_PATCH << std::endl;
+
+        return 0;
+    }
+    else
+    {
+        std::cout << "Wrong arguments. See readme for details" << std::endl;
+    }
 }
 
